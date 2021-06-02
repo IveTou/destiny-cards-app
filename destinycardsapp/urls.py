edfa61a0c0b04d2f68443d4c536027e7c19886f7
Ive <ive_tourinho@hotmail.com>
2021-06-01 22:20:47 -0300

    feat: creating a skeleton website

--- destinycardsapp/urls.py
+++ destinycardsapp/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/catalog/')),
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
 ]
